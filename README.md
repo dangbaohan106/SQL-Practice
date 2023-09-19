420a84aadcf4c42aa44dae6aaa5516fde78a730c
Đặng Bảo Hân <dangbaohan106@gmail.com>
2023-09-19 19:21:46 +0700

    sql_practice

---
+++ README.md
@@ -0,0 +1,30 @@
+## Q1:
+Create Table CHOTOT_2 as follows:  
+- Region is a character data type (supports Unicode) and has a maximum of 150 characters;  
+- Income is a character data type (Unicode is not supported) and has a maximum of 50 characters.  
+![](Untitled1.png)
+## Q2:
+In the Region column, update the values Southeast and DNB to Dong Nam Bo to ensure consistency  
+![](Untitled2.png)
+## Q3:
+How many Contact_Private_Sellers are there for each Platform?  
+![](Untitled3.png)
+## Q4:
+From table CHOTOT and CHOTOT2, calculate the total Contact_Pro_Sellers by each Region with the following conditions:  
+- Only take Regions with High or Medium Income  
+![](Untitled4.png)
+Platform does not include Chotot Desktop  
+![](Untitled4.2.png)
+From the 1st to the 15th of every month or from the 25th of every month onwards  
+![](Untitled4.3.png)
+## Q5:
+- What are the top 3 regions with highest number of private sellers contacts by each platform? ​  
+![](Untitled5.png)
+- What are the top 3 regions that prefer buying from the Pro sellers?  
+![](Untitled6.png)
+- For each day, what are the region with highest number of seller?​  
+![](Untitled7.png)
+## Q6:
+Total sellers of Hanoi​ and HCM. If total sellers of Ha Noi >= 45% HCM, then write ‘HN has more than 45% sellers of HCM’, else write ‘HN has less than 45% sellers of HCM’, name the column HN situation.  
+![](Untitled8.png)
+
---
+++ sql_practice.sql
@@ -0,0 +1,129 @@
+﻿-- 2.		-- (10%) Import file excel dataset Chợ tốt
+				-- (10%) Tạo Table CHOTOT_2 như sau:
+				-- Region là kiểu dữ liệu ký tự (hỗ trợ Unicode) và tối đa 150 ký tự
+				-- Income là kiểu dữ liệu ký tự (không hỗ trợ Unicode) và tối đa 50 ký tự
+CREATE TABLE CHOTOT_2
+(
+	[Region] NVARCHAR(150),
+	[Income] VARCHAR(50)
+)
+GO
+INSERT INTO CHOTOT_2 VALUES
+('Dong Nam Bo','High'),
+('Quang Nam Da Nang','Medium'),
+('Hai Phong - Nam Dinh - Thai Binh','Low'),
+('Tay Nam Bo','Low'),
+('Nam Trung Bo','Medium'),
+('Tay Nguyen','Medium'),
+('Dong Bac','Medium'),
+('Tay Bac','Medium'),
+('Ha Noi','High'),
+('TP HCM','High')
+GO
+
+-------------------------------------------------------------------------------------------------------------------------
+-- 3.		-- Ở cột Region, update các giá trị Đông Nam Bộ và DNB thành Dong Nam Bo để đảm bảo consistent
+UPDATE dbo.[data$]
+SET Region = 'Dong Nam Bo'
+WHERE region = N'Đông Nam Bộ' or region = 'DNB'
+
+-------------------------------------------------------------------------------------------------------------------------
+-- 4.		-- Có bao nhiêu Contact_Private_Sellers theo từng Platform
+SELECT platform
+				, COUNT(Contact_Private_Sellers) AS so_luong
+FROM dbo.[data$]
+GROUP BY platform
+
+-------------------------------------------------------------------------------------------------------------------------
+-- 5.		-- Từ table CHOTOT và CHOTOT2, tính tổng Contact_Pro_Sellers theo từng Regions với điều kiện sau:
+				-- Chỉ lấy Regions có Income High hoặc Medium
+SELECT a.Region
+				, SUM(a.Contact_Pro_Sellers) AS sum_
+FROM dbo.[data$] a
+INNER JOIN dbo.CHOTOT_2 b
+			ON b.Region = a.Region
+WHERE b.Income <> 'low'
+GROUP BY a.Region
+
+ 				-- Platform không bao gồm Chotot Desktop
+ SELECT  a.region
+				, SUM(Contact_Pro_Sellers) AS sum_
+FROM dbo.[data$] a
+INNER JOIN dbo.CHOTOT_2 b
+			ON a.Region = b.Region
+ WHERE platform <> 'Chotot Desktop'
+GROUP BY a.Region
+
+				-- Từ ngày 1 đến ngày 15 hằng tháng hoặc từ ngày 25 hằng tháng trở đi
+ SELECT  a.region
+				, SUM(Contact_Pro_Sellers) AS sum_
+FROM dbo.[data$] a
+INNER JOIN dbo.CHOTOT_2 b
+			ON a.Region = b.Region
+WHERE (DAY(date) BETWEEN 1 AND 15 OR DAY(date) >25)
+GROUP BY a.region
+
+-------------------------------------------------------------------------------------------------------------------------
+-- What are the top 3 regions with highest number of private sellers contacts by each platform? ​
+SELECT TOP 3 region
+				, sum(Contact_Private_Sellers) AS sum_pri
+FROM dbo.[data$]
+GROUP BY Region
+ORDER BY sum_pri DESC
+
+----------------------------------------------------------------
+-- What are the top 3 regions that prefer buying from the Pro sellers? ​
+SELECT  TOP 3 region
+				, CASE WHEN sum(Contact_Pro_Sellers) - sum(Contact_Private_Sellers) < 0 THEN sum(Contact_Pro_Sellers) 
+				END AS diff
+FROM dbo.[data$]
+GROUP BY region
+ORDER BY diff DESC
+
+-- For each day, what are the region with highest number of seller?​
+WITH tam AS (
+							SELECT Region
+											, Date
+											, SUM(Contact_Private_Sellers) + SUM(Contact_Pro_Sellers) AS sum_
+							FROM dbo.[data$]
+							GROUP BY date, region )
+, tam2 AS (
+					SELECT DATE
+									, MAX(sum_) AS highest
+					FROM tam
+					GROUP BY date )
+SELECT a.Region
+				, a.Date
+				, b.highest
+FROM tam a
+INNER JOIN tam2 b
+ON b.Date = a.Date AND b.highest = a.sum_
+
+-------------------------------------------------------------------------------------------------------------------------
+-- (5%) For each month, please make 4 columns:​
+				-- Total sellers of Hanoi​
+				-- Total sellers of TP HCM
+				-- If total sellers of Ha Noi >= 45% HCM, then write ‘HN has more than 45% sellers of HCM’
+				-- else write ‘HN has less than 45% sellers of HCM’, name the column HN situation
+WITH hcm AS (
+SELECT MONTH(date) AS month_
+				, SUM(Contact_Private_Sellers) + SUM(Contact_Pro_Sellers) AS total_hcm
+FROM dbo.[data$]
+WHERE region = 'TP HCM'
+GROUP BY MONTH(date) )
+
+, hanoi AS (
+SELECT MONTH(date) AS month_
+				, SUM(Contact_Private_Sellers) + SUM(Contact_Pro_Sellers) AS total_hanoi
+FROM dbo.[data$]
+WHERE region = 'Hanoi'
+GROUP BY MONTH(date) )
+
+SELECT total_hcm
+				, total_hanoi
+				, CASE WHEN b.total_hanoi >= 0.45 *  a.total_hcm THEN 'HN has more than 45% sellers of HCM' END AS Conclusion
+				, CASE WHEN b.total_hanoi < 0.45 *  a.total_hcm THEN 'HN has less than 45% sellers of HCM' END AS Conclusion2
+FROM hcm a
+INNER JOIN hanoi b
+			on a.month_ = b.month_
+ORDER BY a.month_ ASC
